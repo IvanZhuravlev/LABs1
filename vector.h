a5f542691e13964b0d8a0f1ccbec1868b6880eea
IvanZhuravlev <32226412+IvanZhuravlev@users.noreply.github.com>
2018-03-05 19:36:19 +0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,19 +1,19 @@
-#ifndef VECTOR_H 
-#define VECTOR_H 
-#include <cmath> 
-#include <string.h> 
- 
-typedef struct vector 
-{ 
- int x; 
- int y; 
-}vector; 
- 
-void print_menu(int select); 
-void print_error(); 
-void toString(double r1, double r2); 
-double sum(double a, double b); 
-double substraction(double a, double b); 
-double multiply(double n, double coord); 
- 
-#endif VECTOR_H
+#ifndef VECTOR_H
+#define VECTOR_H
+#include <cmath>
+#include <string.h>
+
+typedef struct vector
+{
+ int x;
+ int y;
+}vector;
+
+void print_menu(int select);
+void print_error();
+void toString(double r1, double r2);
+double sum(double a, double b);
+double substraction(double a, double b);
+double multiply(double n, double coord);
+
+#endif
